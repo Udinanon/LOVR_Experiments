bf95709ec8f73d5baf25d43e5abac16563783123
Udinanon <m.trapanotto@tutamail.com>
2023-03-08 08:43:49 +0100

    Ceanup of motion code Added scaling control Added animation playback control Fix for transfer pass in v0.16+

--- Project/main.lua
+++ Project/main.lua
@@ -22,15 +22,13 @@ function lovr.load()
 
   lovr.graphics.setBackgroundColor(.1, .1, .1, 1)
 
-  motion = {
+  animation_play = true
+  orbit_motion = {
     pose = lovr.math.newMat4(), -- Transformation in VR initialized to origin (0,0,0) looking down -Z
     thumbstickDeadzone = 0.3, -- Smaller thumbstick displacements are ignored (too much noise)
-    dashDistance = 1.5,
-    thumbstickCooldownTime = 0.3,
-    thumbstickCooldown = 0,
-    -- Smooth motion parameters
-    turningSpeed = 2 * math.pi * 1 / 6,
+    -- Smooth orbit_motion parameters
     walkingSpeed = 2,
+    scaleSpeed = 0.02
   }
   
   local cassini = lovr.filesystem.read("cassini_pos.txt")
@@ -50,12 +48,12 @@ function lovr.load()
   print(#cassini_pos)
 
   Graph = Graphs:new()
-  --Graph:setVisible()
+  Graph:setVisible()
   Graph:drawAxes()
 
   BlackBoard = Graphs:new(1, 4)
 --  BlackBoard:drawPoint({1, 1}, {1, 1, 1, 1})
-  --BlackBoard:setVisible()
+  BlackBoard:setVisible()
   BlackBoard:drawAxes()
   BlackBoard:setPose(mat4(vec3(0, 1.5, 1), quat(0, 0, 0, 1)))
 
@@ -70,48 +68,57 @@ function lovr.update(dt)
 
     if lovr.headset.isTracked('left') then
       local x, y = lovr.headset.getAxis('left', 'thumbstick')
-      local head_quat = quat(lovr.headset.getOrientation("head"))
+      local head_quat = quat()
 
       -- Smooth strafe movement
-      if math.abs(x) > motion.thumbstickDeadzone then
+      if math.abs(x) > orbit_motion.thumbstickDeadzone then
         local strafeVector = head_quat:mul(vec3(1, 0, 0))
-        motion.pose:translate(strafeVector * x * motion.walkingSpeed * dt)
+        orbit_motion.pose:translate(strafeVector * x * orbit_motion.walkingSpeed * dt)
       end
       -- Smooth Forward/backward movement
-      if math.abs(y) > motion.thumbstickDeadzone then
-        motion.pose:translate(head_quat:direction() * y * motion.walkingSpeed * dt)
+      if math.abs(y) > orbit_motion.thumbstickDeadzone then
+        orbit_motion.pose:translate(head_quat:direction() * y * orbit_motion.walkingSpeed * dt)
       end
       -- if left trigger is pressed
       if lovr.headset.isDown("left", "trigger") then
           local vertVector = head_quat:mul(vec3(0, 1, 0))
-          motion.pose:translate(vertVector * 0.5 * motion.walkingSpeed * dt)
+          orbit_motion.pose:translate(vertVector * 0.5 * orbit_motion.walkingSpeed * dt)
       end
       -- if left trigger is pressed
       if lovr.headset.isDown("left", "grip") then
         local vertVector = head_quat:mul(vec3(0, 1, 0))
-        motion.pose:translate(vertVector * -0.5 * motion.walkingSpeed * dt)
+        orbit_motion.pose:translate(vertVector * -0.5 * orbit_motion.walkingSpeed * dt)
+      end
+    end
+    if lovr.headset.isTracked('right') then
+      local x, y = lovr.headset.getAxis('right', 'thumbstick')
+      
+      -- Smooth scaling of orbit data
+      if math.abs(y) > orbit_motion.thumbstickDeadzone then
+        orbit_motion.scale = 1.+ orbit_motion.scaleSpeed * Utils.sign(y)
+        orbit_motion.pose:scale(orbit_motion.scale)
       end
     end
 
     if lovr.headset.wasPressed("right", 'trigger') then
-      local curr_color = Utils.shallowCopy(color)
-      Utils.addVector(lovr.math.newVec3(lovr.headset.getPosition("right")), lovr.math.newVec3(lovr.headset.getVelocity("right")), curr_color, true)
-      -- create cube there with color and shift it slightly
-      color[1] = color[1]+40
-      Utils.addBox(vec3(lovr.headset.getPosition("right")))
+      print("c")
     end 
-      
+
+    if lovr.headset.wasPressed("right", "a") then
+      print("b")
+      animation_play = not animation_play
+      print("a", animation_play)
+    end
    
   end
+
+
 
   -- update blackboard
   for i, hand in ipairs(lovr.headset.getHands()) do
     local position = lovr.math.vec3(lovr.headset.getPosition(hand))
     Graph:drawPoint({position.x, position.z}, {10, 1, 1, 1})
   end
-
-  
-
 
   -- when both grips are pressed, kinda finnicky but ok
   if lovr.headset.wasPressed("left", 'grip') and lovr.headset.wasPressed("right", 'grip') then
@@ -143,7 +150,8 @@ end
 function lovr.draw(pass)
   Utils.drawHands(pass, 0xffffff)
   
-  pass:transform(mat4(motion.pose):invert())
+
+  pass:transform(mat4(orbit_motion.pose))
   -- odd flickeringnin v16 from tables, fixed later
   pass:setColor(0xA66300)
   local saturn_transform = mat4(vec3(0, 1, 0), vec3(0.05), quat())
@@ -153,7 +161,9 @@ function lovr.draw(pass)
   pass:line(cassini_pos)
 
   pass:setColor(0x6D7582)
-  cassini_index = (cassini_index % #cassini_pos) + 1
+  if animation_play then 
+    cassini_index = (cassini_index % #cassini_pos) + 1
+  end
   local sat_transform = mat4()
   sat_transform:translate(cassini_pos[cassini_index])
   sat_transform:scale(0.02)
@@ -165,12 +175,12 @@ function lovr.draw(pass)
   --Utils.drawLabels(pass)
 
   -- draw blackboard
-  --local transfer_pass = lovr.graphics.getPass("transfer")
+  local transfer_pass = lovr.graphics.getPass("transfer")
   --Graphs:drawAll(pass, transfer_pass)
   Utils.drawBounds(pass)
   Utils.drawAxes(pass)
   
 
   Utils.drawBoxes(pass)
-  --lovr.graphics.submit({ pass, transfer_pass })
+  return lovr.graphics.submit({ pass, transfer_pass })
 end
