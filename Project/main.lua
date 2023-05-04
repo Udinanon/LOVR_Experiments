5c30fdfd8b290a525096bf2788c3eea956cf9f36
Udinanon <m.trapanotto@tutamail.com>
2023-05-04 17:21:39 +0200

    first version with Bjorn's code for 3d mirror view

--- Project/main.lua
+++ Project/main.lua
@@ -1,122 +1,86 @@
----@diagnostic disable: deprecated
+local mirror = {}
 
-Utils = require "Utils"
-Graphs = require "Graphs"
+function mirror:init()
+  self.head = lovr.math.newMat4()
+  self.views = 2
+  self.width = lovr.system.getWindowWidth() * .5
+  self.height = lovr.system.getWindowHeight()
+  self.ipd = .063
+  self.fovy = .6
+  self.fovx = self.fovy * (self.width / self.height)
+  self.canvas = lovr.graphics.newTexture(self.width, self.height, self.views, {
+    type = 'array',
+    usage = { 'render', 'sample' },
+    mipmaps = false
+  })
 
--- run on boot of the program, where all the setup happes
+  self.stereoShader = lovr.graphics.newShader('fill', [[
+    layout(set = 2, binding = 0) uniform texture2DArray canvas;
+    vec4 lovrmain() {
+      vec2 eyeUV = UV * vec2(2, 1);
+      float eyeIndex = floor(UV.x * 2.);
+      return Color * getPixel(canvas, eyeUV, eyeIndex);
+    }
+  ]])
+end
+
+function mirror:setHeadPose(...)
+  self.head:set(...)
+end
+
+function mirror:render(fn)
+  local pass = lovr.graphics.getPass('render', self.canvas)
+
+  local offset = vec3(self.ipd * .5, 0, 0)
+  pass:setViewPose(1, mat4(self.head):translate(-offset))
+  pass:setViewPose(2, mat4(self.head):translate(offset))
+
+  local projection = mat4():fov(self.fovx, self.fovx, self.fovy, self.fovy, .01)
+  pass:setProjection(1, projection)
+  pass:setProjection(2, projection)
+
+  fn(pass)
+
+  return pass
+end
+
+function mirror:blit(pass)
+  pass:push('state')
+  pass:setShader(self.stereoShader)
+  pass:send('canvas', self.canvas)
+  pass:fill()
+  pass:pop('state')
+end
+
+---
+
+local function draw(pass)
+  pass:setShader('normal')
+  pass:monkey(0, 1.7, -2)
+  pass:cube(1,1, 1)
+end
+
 function lovr.load()
-  print("LODR LOAD")
-  -- prepare for the color wheel thing
-  color = {0, 1, 1, 1}
-  -- this runs the physics, here we also set some global constants
-  world = lovr.physics.newWorld()
-  world:setLinearDamping(.01)
-  world:setAngularDamping(.005)
-  world:newBoxCollider(0, 0, 0, 50, .05, 50):setKinematic(true)
-  --used to track if buttons were pressed
-  State = {["A"] = false, ["B"] = false, ["X"] = false, ["Y"] = false}
-  function State:isNormal ()
-    -- check uf no state is normal
-    return (not State["A"] and not State["B"] and not State["X"] and not State["Y"])
-  end
-
-  lovr.graphics.setBackgroundColor(.1, .1, .1, 1)
-
-
-  Graph = Graphs:new()
-  Graph:setVisible()
-  Graph:drawAxes()
-
-  BlackBoard = Graphs:new(1, 4)
---  BlackBoard:drawPoint({1, 1}, {1, 1, 1, 1})
-  BlackBoard:setVisible()
-  BlackBoard:drawAxes()
-  BlackBoard:setPose(mat4(vec3(0, 1.5, 1), quat(0, 0, 0, 1)))
-
+  ANDROID = lovr.system.getOS() == 'Android'
+  mirror:init()
 end
 
--- runs at each dt interval, where you do input and physics
 function lovr.update(dt)
-  -- update physics, like magic
-  world:update(dt)
-
-  if State:isNormal() then
-    if lovr.headset.wasPressed("right", 'trigger') then
-      local curr_color = Utils.shallowCopy(color)
-      Utils.addVector(lovr.math.newVec3(lovr.headset.getPosition("right")), lovr.math.newVec3(lovr.headset.getVelocity("right")), curr_color, true)
-      -- create cube there with color and shift it slightly
-      color[1] = color[1]+40
-      Utils.addBox(vec3(lovr.headset.getPosition("right")))
-    end 
-      
-      -- if left trigger is pressed
-    if lovr.headset.wasPressed("left", "trigger") then
-      print("Left Click")
-      
-      Graph:reposition()
-    end
-  end
-
-  -- update blackboard
-  for i, hand in ipairs(lovr.headset.getHands()) do
-    local position = lovr.math.vec3(lovr.headset.getPosition(hand))
-    Graph:drawPoint({position.x, position.z}, {10, 1, 1, 1})
-  end
-
-  
-
-
-  -- when both grips are pressed, kinda finnicky but ok
-  if lovr.headset.wasPressed("left", 'grip') and lovr.headset.wasPressed("right", 'grip') then
-    -- clear all
-      Graph:clean()
-      BlackBoard:clean()
-      Utils.boxes = {}
-  end
-
-  if lovr.headset.wasPressed("right", "a") then
-    State["A"] = not State["A"]
-    if State["A"] then
-
-      
-    end
-  end
-  if lovr.headset.wasPressed("right", "b") then
-    State["B"] = not State["B"]
-    if State["B"] then
-
-
-    end
-  end
-
-
+  mirror:setHeadPose(lovr.headset.getPose())
 end
 
--- this draws obv
 function lovr.draw(pass)
-
-
-  Utils.drawVectors(pass)
-  --Utils.drawLabels(pass)
-
-  
-  -- draw hands
-  if State:isNormal() then
-    Utils.drawHands(pass, 0xffffff)
+  if ANDROID then
+    return draw(pass)
+  else
+    return lovr.graphics.submit(mirror:render(draw))
   end
-  if State["A"] then
-    Utils.drawHands(pass, 0x0000ff)
-  end
-  if State["B"] then
-    Utils.drawHands(pass, 0x00ff00)
-  end
-  -- draw blackboard
-  local transfer_pass = lovr.graphics.getPass("transfer")
-  Graphs:drawAll(pass, transfer_pass)
-  Utils.drawBounds(pass)
-  Utils.drawAxes(pass)
-  
+end
 
-  Utils.drawBoxes(pass)
-  lovr.graphics.submit({ pass, transfer_pass })
+function lovr.mirror(pass)
+  if ANDROID then
+    return true
+  else
+    mirror:blit(pass)
+  end
 end
