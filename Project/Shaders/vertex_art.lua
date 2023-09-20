59001d40501e74e089a0bc33144a47a3ad5353cf
Udinanon <m.trapanotto@tutamail.com>
2023-09-20 16:47:28 +0200

    Stable audio texture source, issues perhaps with FFT?

--- Project/Shaders/vertex_art.lua
+++ Project/Shaders/vertex_art.lua
@@ -3,9 +3,7 @@ local vertex_art = {}
 vertex_art.FFT = require("fft")
 
 ---TODO:
--- Preprocess shaders to ensure comatibility with minimal manual editing
 -- Reorder code to minimize GPU actions
--- Setup dummy samplers for sound and touch
 -- Make a trurly 3D shader
 -- Showcase and share
 -- Contact Gman
@@ -27,7 +25,7 @@ end
 
 
 function vertex_art:init()
-    vertex_art.shader_file = "Shaders/vertex_art/wave_vs.vert"
+    vertex_art.shader_file = "Shaders/vertex_art/slash.vert"
     local prepared_shader = self:prepare_shader(vertex_art.shader_file)
     print(prepared_shader)
     lovr.filesystem.write("Shaders/vertex_art/processed.vert", prepared_shader) -- not working?
@@ -40,41 +38,53 @@ function vertex_art:init()
     vertex_art.sample_rate = vertex_art.music:getSampleRate()
     vertex_art.frame_size = vertex_art.sample_rate / 60
     vertex_art.audio_offset = 0
-    vertex_art.sound_image = lovr.data.newImage(vertex_art.time_samples, vertex_art.fft_samples, "r8")
+    vertex_art.sound_image = lovr.data.newImage(vertex_art.time_samples, vertex_art.fft_samples, "rgba8")
     -- sound has values 0-255 linearly
     vertex_art.sound_texture = lovr.graphics.newTexture(vertex_art.sound_image,
-        { format = "r8", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
+        { format = "rgba8", linear = false, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
+    vertex_art.float_sound_texture = lovr.graphics.newTexture(vertex_art.fft_samples, 240,
+        { format = "rgba32f", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
+    vertex_art.volume_texture = lovr.graphics.newTexture(4, 240,
+        { format = "rgba8", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
+    vertex_art.touch_texture = lovr.graphics.newTexture(32, 240,
+        { format = "rgba8", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
+
+
+    material = lovr.graphics.newMaterial({texture = vertex_art.sound_texture})
+    vertex_art.music_source = lovr.audio.newSource(vertex_art.music)
+    vertex_art.music_source:play()
 end
 
 function vertex_art:load(pass)
-    vertex_art.audio_offset = vertex_art.audio_offset + lovr.headset.getDeltaTime()
-    for x = 1, vertex_art.time_samples do 
-        local sound_buf, _ = vertex_art.music:getFrames(vertex_art.fft_samples, vertex_art.audio_offset + vertex_art.frame_size*(x-1))
-        local byte_fft = vertex_art.FFT.byte_real_fft(sound_buf)
-        for y = 1, vertex_art.fft_samples do
-            vertex_art.sound_image:setPixel(x-1, y-1, byte_fft[y])
-        end
+    --vertex_art.audio_offset = vertex_art.audio_offset + lovr.headset.getDeltaTime()
+    vertex_art.audio_offset = vertex_art.audio_offset + vertex_art.frame_size
+    vertex_art.sound_image:paste(vertex_art.sound_image, 0, 0, 1, 0, vertex_art.time_samples - 1, vertex_art.fft_samples)
+    --for x = 1, vertex_art.time_samples do
+    local sound_buf, _ = vertex_art.music:getFrames(vertex_art.fft_samples,
+            vertex_art.audio_offset )
+    --+ vertex_art.frame_size * (vertex_art.time_samples- 1)
+    local byte_fft = vertex_art.FFT.byte_real_fft(sound_buf)
+    for y = 1, vertex_art.fft_samples do
+        vertex_art.sound_image:setPixel(vertex_art.time_samples - 1, y - 1, byte_fft[y], byte_fft[y], byte_fft[y],
+            byte_fft[y])
     end
+    --end
+
 
 -- empty image
     --local sound_texture = lovr.graphics.newTexture(fft_samples, 240,        { format = "r8", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
     -- floatSound instad uses negative decibels, which is bullshit
-    local float_sound_texture = lovr.graphics.newTexture(vertex_art.fft_samples, 240,
-        { format = "rgba32f", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
-    local volume_texture = lovr.graphics.newTexture(4, 240,
-        { format = "rgba8", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
-    local touch_texture = lovr.graphics.newTexture(32, 240,
-        { format = "rgba8", linear = true, samples = 1, mipmaps = false, usage = { "sample", "render", "transfer" } })
-    pass:setShader(self.shader)
+
+    pass:setShader(vertex_art.shader)
     pass:setColor(1, 1, 1, 1)
     pass:send("time", lovr.timer.getTime())
     pass:send("resolution", vec2(1000, 1000))
     pass:send("mouse", vec2(0, 0));
     pass:send("background", vec4(1, 1, 1, 1));
-    pass:send("touch", touch_texture)
+    pass:send("touch", vertex_art.touch_texture)
     pass:send("sound", vertex_art.sound_texture)
-    pass:send("floatSound", float_sound_texture)
-    pass:send("volume", volume_texture)
+    pass:send("floatSound", vertex_art.float_sound_texture)
+    pass:send("volume", vertex_art.volume_texture)
     pass:send("soundRes", vec2(1000, 1000));
     pass:send("_dontUseDirectly_pointSize", 1.);
 end
@@ -82,10 +92,14 @@ end
 
 function vertex_art.update_textures(transfer_pass)
     transfer_pass:copy(vertex_art.sound_image, vertex_art.sound_texture)
+    --vertex_art.sound_texture:setPixels(vertex_art.sound_image)
 end
 
 
 function vertex_art:demo(pass, transfer_pass)
+
+
+
     vertex_art:load(pass)
     vertex_art.update_textures(transfer_pass)
     local indexCount = 1000
@@ -110,6 +124,9 @@ function vertex_art:demo(pass, transfer_pass)
     --- nothing different from line
     --pass:circle(mat4(vec3(1, 1, 1)), "line", 0, 2 * math.pi, indexCount)
     pass:setShader()
+    pass:setMaterial(material)
+    pass:cube()
+    pass:setMaterial()
 end
 
 return vertex_art
